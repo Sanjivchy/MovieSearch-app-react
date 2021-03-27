4204a13d0e51686afcd096c61b3e4010c23b7a73
xoonziip <sanjivchaudhary416@gmail.com>
2021-03-27 13:24:41 +0545

    created search button

---
+++ src/components/SearchMovies.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SearchMovies() {
+    return (
+       <form className="form" onSubmit="">
+           <label className="label" htmlFor="query">Movie Name</label>
+           <input className= "input" type="text" name="query" placeholder="i.e. Jurassic Park"/>
+            <button className=" button">Search</button>
+       </form>
+    )
+}
