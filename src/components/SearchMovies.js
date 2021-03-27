e148e64ecc5f81a338a8d86219c2b3a5777a749a
xoonziip <sanjivchaudhary416@gmail.com>
2021-03-27 14:05:30 +0545

    Api Fetched from themoviesdb.org

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function SearchMovies() {
+export default function SearchMovies(){
+    
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitting");
+        const query = "Jurassic Park";
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=93bdff21bc780c3887d38fc5858c9b6a&language=en-US&query=${query}&page=1&include_adult=false`;
+   
+        try {
+            const res = await fetch(url);
+            const data  = await res.json();
+            console.log(data);
+        }catch(err){
+            console.error(err);
+        }
+    }
+    
     return (
-       <form className="form" onSubmit="">
-           <label className="label" htmlFor="query">Movie Name</label>
-           <input className= "input" type="text" name="query" placeholder="i.e. Jurassic Park"/>
-            <button className="button">Search</button>
-       </form>
+        <form className="form" onSubmit={searchMovies}>
+            <label className="label" htmlFor="query">Movie Name</label>
+            <input className="input" type="text" name="query"
+                placeholder="i.e. Jurassic Park"/>
+            <button className="button" type="submit">Search</button>
+        </form>
     )
 }
